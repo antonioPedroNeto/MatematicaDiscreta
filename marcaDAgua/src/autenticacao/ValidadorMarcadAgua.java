91e48d1b7808640e5dd562eb94acd77ed44b5d07
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2016-04-15 18:35:27 -0300

    Retirada do metodo main, Adicionado comentarios, troca no tipo de retorno e dois parametros de entrada

--- marcaDAgua/src/autenticacao/ValidadorMarcadAgua.java
+++ marcaDAgua/src/autenticacao/ValidadorMarcadAgua.java
@@ -15,26 +15,14 @@ public class ValidadorMarcadAgua {
 	
 	private final Color COR_DE_FUNDO = Color.BLACK;
 	
-	public static void main(String[] args) {
-
-		ValidadorMarcadAgua m = new ValidadorMarcadAgua();//criar um objeto do tipo autenticador
-		try {
-			m.image();//chama o metodo que vai percorrer a imagem
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
 	/**
 	 * Metodo que percorre uma imagem como um vetor e mostra o rgb de cada pixel
 	 * @throws InterruptedException
 	 */
-	public boolean image() throws InterruptedException{
+	public boolean image(String imagemCandidata, String imagemDeComparacao) throws InterruptedException{
 		
-		File imagemPreta = new File("//home/aramissa/Desktop/mehAguaBranca.png");//cria um objeto do tipo file para guardar o caminho da imagem
-		File imagemMarcada = new File("//home/aramissa/Desktop/mehAgua.png");//cria um objeto do tipo file para guardar o caminho da imagem
+		File imagemPreta = new File(imagemDeComparacao);//cria um objeto do tipo file para guardar o caminho da imagem
+		File imagemMarcada = new File(imagemCandidata);//cria um objeto do tipo file para guardar o caminho da imagem
 
 		Color imagemPretaColor; //cria uma referencia do tipo Color
 		double pixeisTotal = 0;
@@ -62,30 +50,31 @@ public class ValidadorMarcadAgua {
 							//System.out.println(pixeisDiff);
 							//throw new Exception("Eita! Tem nao!");
 							
-						}
-					}
-				}
-			}
-				System.out.println(pixeisDiff+"   "+pixeisTotal);
-				double media = (pixeisDiff / pixeisTotal) * 100;
-				if(media >= 30){
-					System.out.println("Imagem nao contem a marca!");
-					System.out.println(String.format("Margem de erro: %.2f %%", media));
-				}
-				else{System.out.println("Yay, tem marca dagua!!");
+						}//fecha if
+						
+					}//fecha if
+					
+				}//fecha for j
+				
+			}//fecha for i
+			
+			System.out.println("pixels diferentes:"+pixeisDiff+"   total de pixels percorridos:"+pixeisTotal);
+			double media = (pixeisDiff / pixeisTotal) * 100; //calcula a media de erros ao redor da imagem
+			if(media >= 30){
+				System.out.println("Imagem nao contem a marca!");
 				System.out.println(String.format("Margem de erro: %.2f %%", media));
-				}
+			}else{
+				System.out.println("Yay, tem marca dagua!!");
+				System.out.println(String.format("Margem de erro: %.2f %%", media));
+			}//fecha else
 					//System.out.println("posicao: ("+i+","+j+")"+imagemPretaColor);
 					//return true;			
 			
 		} catch (Exception e) {
 			System.out.println("erro: "+ e.getMessage());//mostra a mensagem caso tenha erro
 			return false;
-		}
-		return true;
-		
-		
-		
-	}
+		}//fecha try-catch
+		return true;		
+	}//fecha image
 
-}
+}//fecha class
